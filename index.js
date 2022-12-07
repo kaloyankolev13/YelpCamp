a0d59a5ff5dbac3244b81e8f6cf6258bfe578302
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2022-12-07 14:07:41 +0100

    campgrounds added post route and get by id

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.render('home');
 });
@@ -26,20 +28,23 @@ app.get('/campgrounds', async (req, res) => {
   res.render('campgrounds/index', { campgrounds });
 });
 
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+  // res.send(req.body);
+
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
 // for single campground
 app.get('/campgrounds/:id', async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
   res.render('campgrounds/show', { campground });
-});
-
-app.get('/makecampground', async (req, res) => {
-  const camp = new Campground({
-    title: 'My Backyard',
-    description: 'cheap camping!',
-  });
-  await camp.save();
-  res.send(camp);
 });
 
 app.listen(3000, () => {
