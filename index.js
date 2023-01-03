fa6898831c19b6a34840dccac126fc1ad90efa37
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-01-03 15:36:44 +0200

    added boiler plate

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const path = require('path');
 
@@ -16,6 +17,7 @@ db.once('open', () => {
 });
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
