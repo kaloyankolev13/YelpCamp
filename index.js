61f83e54bb0aaacaa4f50b89db325c87347ef33a
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-01-15 16:40:57 +0200

    added template for errors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
-// const ExpressError = require('./utils/ExpressError');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const path = require('path');
 
@@ -45,7 +45,8 @@ app.post(
   '/campgrounds',
   catchAsync(async (req, res, next) => {
     // res.send(req.body);
-
+    if (!req.body.campground)
+      throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -91,8 +92,14 @@ app.delete(
   })
 );
 
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page Not Found', 404));
+});
+
 app.use((err, req, res, next) => {
-  res.send('Something went wrong');
+  const { status = 500 } = err;
+  if (!err.message) err.message = 'Something Went Wrong!';
+  res.status(status).render('error', { err });
 });
 
 app.listen(3000, () => {
