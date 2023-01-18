965052664f7e461a52278cb3d84aca557454158e
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-01-18 11:50:37 +0200

    created the mongoose relationship and post route for REVIEWS

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Campground = require('./models/campground');
 const ejsMate = require('ejs-mate');
 const { campgroundSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const path = require('path');
+const Campground = require('./models/campground');
+const Review = require('./models/review');
+const campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -56,8 +58,6 @@ app.post(
   '/campgrounds',
   validateCampground,
   catchAsync(async (req, res, next) => {
-    // if (!req.body.campground)
-    //   throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -101,6 +101,22 @@ app.delete(
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+  })
+);
+
+// Review routes
+
+app.post(
+  '/campgrounds/:id/reviews',
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    console.log(campground);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    console.log(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
   })
 );
 
