744bb208fef88d03f2cd788f3f3858bb25182d4d
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-01-19 15:36:11 +0200

    created a flash message with bootstrap and addded it for completion of the routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -65,6 +65,7 @@ router.put(
     const campground = await Campground.findByIdAndUpdate(id, {
       ...req.body.campground,
     });
+    req.flash('success', 'Successfully updated');
     res.redirect(`/campgrounds/${campground._id}`);
   })
 );
