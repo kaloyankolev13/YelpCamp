a7bcbcb5c850770c9cd25932d5f468d2f5989847
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-01-19 14:47:35 +0200

    created a public folder to serve static files

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/campgrounds', cammpgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
