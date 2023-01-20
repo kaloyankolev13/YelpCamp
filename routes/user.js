e5985fe6b2cc3bfe2f4fddb5708f910e1b99c819
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-01-20 15:51:31 +0200

    added logout route

--- routes/user.js
+++ routes/user.js
@@ -40,4 +40,15 @@ router.post(
   }
 );
 
+router.get('/logout', (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    } else {
+      req.flash('success', 'Logged out');
+      res.redirect('./campgrounds');
+    }
+  });
+});
+
 module.exports = router;
