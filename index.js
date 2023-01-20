e470ae9678aa37029978643d9347cde5991d0a0b
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-01-20 13:49:57 +0200

    set up a passoport in the app

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const path = require('path');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const cammpgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -43,10 +46,23 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
   next();
+});
+
+app.get('/fakeuser', async (req, res) => {
+  const user = new User({ email: 'kaloyan@abv.bg', username: 'Kaloyan' });
+  const newUser = await User.register(user, 'monkey');
+  res.send(newUser);
 });
 
 app.use('/campgrounds', cammpgrounds);
