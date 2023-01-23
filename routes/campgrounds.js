16fb2a840e7cb34b676f90288c4c672f48391fd2
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-01-23 14:20:45 +0200

    Created the authorisation middleware and moved the middleware to the corresponding file

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,20 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
-const { campgroundSchema } = require('../schemas.js');
-const { isLoggedIn } = require('../middleware');
-
-const validateCampground = (req, res, next) => {
-  const { error } = campgroundSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(',');
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
 router.get(
   '/',
@@ -60,6 +48,7 @@ router.get(
 router.get(
   '/:id/edit',
   isLoggedIn,
+  isAuthor,
   catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
@@ -67,6 +56,7 @@ router.get(
       req.flash('error', 'NonExistend Campground');
       return res.redirect('/campgrounds');
     }
+
     res.render('campgrounds/edit', { campground });
   })
 );
@@ -74,6 +64,7 @@ router.get(
 router.put(
   '/:id',
   isLoggedIn,
+  isAuthor,
   validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
@@ -88,6 +79,7 @@ router.put(
 router.delete(
   '/:id',
   isLoggedIn,
+  isAuthor,
   catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 
+const { validateReview } = require('../middleware');
+
 const catchAsync = require('../utils/catchAsync');
-const { reviewSchema } = require('../schemas.js');
 
 const Campground = require('../models/campground');
 const Review = require('../models/review');
-
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(',');
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
 
 router.post(
   '/',
