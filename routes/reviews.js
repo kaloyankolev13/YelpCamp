ccfd3c65378249b95b99b3db858560fb90ac23b1
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-01-23 15:56:51 +0200

    restructured the routes by using router.route and chaining the requests by the same route

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
-
 const catchAsync = require('../utils/catchAsync');
-
 const reviews = require('../controllers/reviews');
 
 router.post('/', validateReview, isLoggedIn, catchAsync(reviews.createReview));
--- routes/user.js
+++ routes/user.js
@@ -4,21 +4,22 @@ const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
 const users = require('../controllers/users');
 
-router.get('/register', users.renderRegister);
+router
+  .route('/register')
+  .get(users.renderRegister)
+  .post(catchAsync(users.registerUser));
 
-router.post('/register', catchAsync(users.registerUser));
-
-router.get('/login', users.renderLogin);
-
-router.post(
-  '/login',
-  passport.authenticate('local', {
-    failureFlash: true,
-    failureRedirect: '/login',
-    keepSessionInfo: true,
-  }),
-  users.loginUser
-);
+router
+  .route('/login')
+  .get(users.renderLogin)
+  .post(
+    passport.authenticate('local', {
+      failureFlash: true,
+      failureRedirect: '/login',
+      keepSessionInfo: true,
+    }),
+    users.loginUser
+  );
 
 router.get('/logout', users.logoutUser);
 
